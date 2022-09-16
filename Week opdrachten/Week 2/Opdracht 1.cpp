d34f11905d705e7187d772d654b92e9b21a2488a
Janine Keek <s1184452@student.windesheim.nl>
2022-09-16 18:02:53 +0200

    Opdracht 2.5 maar X Y Z kan ik nog niet fiksen

--- Week opdrachten/Week 2/Opdracht 1.cpp
+++ Week opdrachten/Week 2/Opdracht 1.cpp
@@ -3,11 +3,11 @@
 
 int main(void)
 {
-	printf("Enter three integers: "); 
-	
+	printf("Enter three integers: ");
+
 	int x, y, z; //declare variables
-	scanf("%d%d%d , &x &y &z "); //read three integers
-	
+	scanf_s("%d%d%d , &x &y &z "); //read three integers
+
 	int result = x * y * z; // multiply the values
 	printf(" The product is %d\n", result); //display the result
 
