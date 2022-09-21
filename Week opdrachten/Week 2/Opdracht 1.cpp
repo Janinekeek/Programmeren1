49c04e3a09343cd7aff640a01476f53e6496f9fe
Janine Keek <s1184452@student.windesheim.nl>
2022-09-21 09:42:52 +0200

    Nu werkt ie wel!

--- Week opdrachten/Week 2/Opdracht 1.cpp
+++ Week opdrachten/Week 2/Opdracht 1.cpp
@@ -6,7 +6,7 @@ int main(void)
 	printf("Enter three integers: ");
 
 	int x, y, z; //declare variables
-	scanf_s("%d%d%d , &x &y &z "); //read three integers
+	scanf_s("%d%d%d", &x, &y, &z); //read three integers
 
 	int result = x * y * z; // multiply the values
 	printf(" The product is %d\n", result); //display the result
